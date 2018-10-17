7d702aa2ca56f910b7d8199bf6d2320fd6991a37
Kyle1668 <kyledevinobrien1@gmail.com>
2018-10-16 20:41:22 -0700

    added binary tree start

--- data structures/binary_tree/binary_tree.py
+++ data structures/binary_tree/binary_tree.py
@@ -10,11 +10,28 @@ class BinaryTree:
         }
 
     def add_node(self, data):
-        if (root_node is None):
-            root_node = self.init_node(data)
+        if (self.root_node is None):
+            self.root_node = self.init_node(data)
         else:
+            new_node = self.init_node(data)
+            self.add_node_helper(new_node, self.root_node)
 
     def add_node_helper(self, new_node, parent_node):
-        if (parent_node.left_node is None):
-            parent_node.left_node = new_node
-        elif (parent_node.data < new_node)
+        if (parent_node is None):
+            parent_node = new_node
+        elif (parent_node["data"] > new_node["data"]):
+            self.add_node_helper(new_node, parent_node["left_node"])
+        else:
+            self.add_node_helper(new_node, parent_node["right_node"])
+
+
+x = BinaryTree()
+
+x.add_node(40)
+x.add_node(19)
+x.add_node(50)
+x.add_node(7)
+x.add_node(30)
+x.add_node(28)
+x.add_node(60)
+
